699945423ccf5f5cc7a47df3828ceb64e1244d81
Pradeep Srivastav <pradeep.srivastav@rsystems.com>
2025-03-31 16:58:14 +0530

    support custom delimiter

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -1,7 +1,15 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
-    let numArray = numbers.split(/,|\n/).map(Number);
+    let delimiter = /,|\n/;
+        
+    if (numbers.startsWith("//")) {
+        let parts = numbers.split("\n", 2);
+        delimiter = new RegExp(parts[0].slice(2));
+        numbers = parts[1];
+    }
+
+    let numArray = numbers.split(delimiter).map(Number);
 
     return numArray.reduce((sum, num) => sum + num, 0);
 }
--- calculator/src/calculator.test.js
+++ calculator/src/calculator.test.js
@@ -15,3 +15,7 @@ test("test for single value", () => {
 test("test with delimiter", () => {
     expect(add("1\n2,3")).toBe(6);
 });
+
+test("supports custom delimiter", () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
