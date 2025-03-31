d0fba8f09b3850f2b5d3cdc982118390017c2726
Pradeep Srivastav <pradeep.srivastav@rsystems.com>
2025-03-31 16:46:32 +0530

    test with dilimiters

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -1,9 +1,11 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
-    let numArray = numbers.split(',').map(Number);
+    let numArray = numbers.split(/,|\n/).map(Number);
 
     return numArray.reduce((sum, num) => sum + num, 0);
 }
+
+// console.log(add('1\n2,3,4'));
 
 module.exports = add;
--- calculator/src/calculator.test.js
+++ calculator/src/calculator.test.js
@@ -7,3 +7,11 @@ test("Return 0 for empty string", () => {
 test("returns sum of two numbers comma saprated values", () => {
     expect(add("1,2")).toBe(3);
 });
+
+test("test for single value", () => {
+    expect(add("1")).toBe(1);
+});
+
+test("test with delimiter", () => {
+    expect(add("1\n2,3")).toBe(6);
+});
